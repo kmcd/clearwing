8e0cdaa4c3608b157abf418314402bc0e8ecfab6
je <je@bixly.com>
2012-08-12 18:15:35 +0800

    generate random 15days, gather data

--- clearwing/utils.py
+++ clearwing/utils.py
@@ -1,4 +1,5 @@
 from pandas import *
+from datetime import datetime
 
 def create_hdf5(fname):
     store = HDFStore(fname + '.h5')
@@ -6,3 +7,31 @@ def create_hdf5(fname):
     
 def save_object(store, obj, key):
     store[key] = obj
+    
+def gen_lkbk_days(today=None, day_list=None, lkbk=3):
+    # generate lkbk_days for the given day(s)
+    seen = set()
+    with_lkbk = []
+    if today is not None:
+        for i in range(lkbk,0,-1):
+            lkbk_day = today - i*datetools.BDay()
+            if lkbk_day in seen: continue
+            seen.add(lkbk_day)
+            with_lkbk.append(lkbk_day)
+    elif day_list is not None:
+        for day in day_list:
+            for i in range(lkbk,0,-1):
+                lkbk_day = day - i*datetools.BDay()
+                if lkbk_day in seen: continue
+                seen.add(lkbk_day)
+                with_lkbk.append(lkbk_day)
+            if day in seen: continue
+            seen.add(day)
+            with_lkbk.append(day)
+    days_str = [date.date().strftime('%Y%m%d') for date in with_lkbk]
+    return days_str
+
+def _print(f, string):
+    print string
+    f.write(string+'\n')
+    
--- prepare_data_random.py
+++ prepare_data_random.py
@@ -1,3 +1,11 @@
+"""
+usage:
+python prepare_data_random.py <set_num> <lkbk>
+
+default:
+    set_num = 1
+    lkbk = 3
+"""
 # Get data by date
 # Pick a day at random from data set
 from random import sample
@@ -9,17 +17,34 @@ import os, glob, sys
 
 # for debugging/printing purposes
 set_printoptions(max_rows=100, max_columns=200, max_colwidth=10000)
-dev_mode = False
+
+# parse inputs to script
+set_num = 1 if not len(sys.argv) > 1 else int(sys.argv[1])
+lkbk = 3 if not len(sys.argv) > 2 else int(sys.argv[1])
 
 # Generate series of business days from QQQ earliest date to QQQ latest date
 qqq_start = datetime(1999,3,10)
 qqq_end = datetime(2012,7,19) - 60 * datetools.BDay()
 trading_days = date_range(qqq_start, qqq_end, freq='B')
 
-# Pick 350 days at random
-training_set = sample(trading_days, 60)
+# Pick 15 days at random
+training_set = sample(trading_days, 15)
 training_set.sort()
 training_set_str = [date.date().strftime('%Y%m%d') for date in training_set]
+
+# save to hdf5 format
+dir_name = 'data/training'
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+store = utils.create_hdf5(dir_name+'/dataset_'+str(set_num))
+
+# save dates to file
+f = open(dir_name+'/dates_set_'+str(set_num)+'.txt', 'w')
+for date in training_set_str:
+    f.write(date+'\n')
+f.close()
+
+training_set_str = utils.gen_lkbk_days(day_list=training_set)
 
 components = {}
 qqq_components = []
@@ -33,10 +58,7 @@ for date in training_set_str:
     start_of_day = datetime.strptime(date,'%Y%m%d').replace(hour=9,minute=30)
     end_of_day = datetime.strptime(date,'%Y%m%d').replace(hour=16)
     idx = date_range(start_of_day, end_of_day, freq='Min')
-    
-    if dev_mode:
-        ct = 0
-        
+            
     # Collect nasdaq components of the given date
     for nasdaq_100_file in glob.glob(os.path.join('data','nasdaq_100','allstocks_'+date,'*')):
         name = nasdaq_100_file.rpartition('_')[2][:-4]
@@ -49,10 +71,6 @@ for date in training_set_str:
                     components[name] = [df]
                 else:
                     components[name].append(df)
-                if dev_mode:
-                    ct = ct + 1
-                    if ct == 3:
-                        break
         except:
             print sys.exc_info()
             print 'error in %s' % nasdaq_100_file
@@ -69,6 +87,7 @@ for date in training_set_str:
 nasdaq_comp = {}
 for k, v in components.items():
     nasdaq_comp[k] = concat(v).fillna(method='pad').fillna(method='bfill')
+    print nasdaq_comp[k].ix[:10,:10]
 nasdaq_comp = Panel(nasdaq_comp)
 
 print '\n\n>>> Nasdaq comp'
@@ -79,18 +98,7 @@ print '\n\n>>> QQQ'
 print qqq.head()
 print qqq.tail()
 
-if not dev_mode:
-    # save to hdf5 format
-    dir_name = 'data/training'
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-    set_num = 1 if not len(sys.argv) > 1 else int(sys.argv[1])
-    f = open(dir_name+'/dates_set_'+str(set_num)+'.txt', 'w')
-    for date in training_set_str:
-        f.write(date+'\n')
-    f.close()
-    store = utils.create_hdf5(dir_name+'/dataset_'+str(set_num))
-    utils.save_object(store, nasdaq_comp, 'nasdaq_comp')
-    utils.save_object(store, qqq, 'qqq')
-    print store
+utils.save_object(store, nasdaq_comp, 'nasdaq_comp')
+utils.save_object(store, qqq, 'qqq')
+print store
 
