5e067d95562d0755f3d57fd87eb6afd47cb33a0a
je <je@bixly.com>
2012-08-15 10:29:30 +0800

    use 60consecutive days for outofsampletest

--- out_of_sample_test.py
+++ out_of_sample_test.py
@@ -1,4 +1,7 @@
 """
+use prepare_data.py to gather data for 60 consecutive days starting at a 
+random date
+
 usage:
 python out_of_sample_test.py <date> <lkbk> <ntop> <k>
 
@@ -10,7 +13,7 @@ from datetime import datetime
 from pandas import *
 import sys, time
 
-lkbk = 5 if not len(sys.argv) > 2 else sys.argv[2]
+lkbk = 3 if not len(sys.argv) > 2 else sys.argv[2]
 ntop = 10 if not len(sys.argv) > 3 else sys.argv[3]
 k = 7 if not len(sys.argv) > 4 else sys.argv[4]
 
@@ -20,81 +23,135 @@ store = HDFStore(dir_name+'/'+start_day_str+'.h5')
 
 nasdaq_comp = store['nasdaq_comp']
 qqq = store['qqq']
+vol_mat = store['vol_mat']
+liq_mat = store['liq_mat']
 
-qqq_long = {}
-qqq_short = {}
-for i in range(len(qqq)):
-    t = qqq.index[i]
-    if t.hour == 16:
-        continue
-    qqq_long[t] = select_model.is_long(qqq, t)
-    qqq_short[t] = select_model.is_short(qqq, t)
-    
-qqq['is_long'] = Series(qqq_long)
-qqq['is_short'] = Series(qqq_short)
-qqq['is_long'].fillna(value=False)
-qqq['is_short'].fillna(value=False)
-print qqq.head()
+def set_start_time(dates):
+    """
+    set all date's time to 9:30
+    """
+    return dates.shift(9, freq='H').shift(30, freq='Min')
 
 start_day = datetime.strptime(start_day_str, '%Y%m%d')
 training_set = date_range(start_day, periods=60, freq='B')
 training_set_str = [date.date().strftime('%Y%m%d') for date in training_set]
+training_set = set_start_time(training_set)
 
-# compute for liquidity (Volume * Close)
-# converted to per million units for printing
-close_price_mat = nasdaq_comp.ix[:,:,'Close']
-vol_mat = nasdaq_comp.ix[:,:,'Volume']
-liq_mat = close_price_mat * vol_mat / 1000000
-liq_mat = liq_mat.fillna(value=0)
-print '\n\n>>> Nasdaq Liquidity in millions'
-for i in range(0,len(liq_mat.columns),10):
-    print liq_mat.ix[:5,i:i+10]
-
-utils.save_object(store, vol_mat, 'vol_mat')
-utils.save_object(store, liq_mat, 'liq_mat')
-
-# select liquidity at the end of the each day
-training_days = date_range(training_set[lkbk], training_set[-1], freq='B').shift(16, freq='H')
-liq_mat_eod = liq_mat.reindex(training_days)
-
-# collect top nasdaq components in terms of liquidity
-top_dims = []
-with_records = []
-for i in range(0, len(liq_mat_eod)):
-    try:
-        row = liq_mat_eod.ix[i,:]
-        new_index = row.index[np.argsort(row)[:-(ntop+1):-1]]
-        row = row.reindex(index=new_index)
-        top_dims_day = DataFrame({'Liquidity':row})
-        top_dims.append(top_dims_day)
-        with_records.append(liq_mat_eod.index[i])
-    except:
-        print 'no record on %s, maybe a holiday' % liq_mat_eod.index[i]
-        
-top_dims = concat(top_dims, keys=with_records)
-topn_liq = select_model.get_top_dims(liq_mat, top_dims, training_set[0], training_days[0], top=ntop)
+def set_start_time(dates):
+    """
+    set all date's time to 9:30
+    """
+    return dates.shift(9, freq='H').shift(30, freq='Min')
     
-"""
-print '\n\n>>> Top %d liquidity per day' % ntop
-print top_dims.head(10)
-print top_dims.tail(10)
+def day_time_range(date):
+    """
+    generate range of time from 9:30 to 16:00 for the given date
+    """
+    return date_range(date.replace(hour=9,minute=30),
+                      date.replace(hour=16,minute=0),
+                      freq='Min')
 
-print '\n\n>>> Top %d liquidity of day %d, %s' % (ntop, lkbk, training_days[0])
-print top_dims.ix[training_days[0]].head(15)
-print top_dims.ix[training_days[0]].index
+lkbk = 3
+ntop = 10
 
-print '\n\n>>> Top %d Nasdaq components with highest liquidity on day %d' % (ntop, lkbk)
-print topn_liq.head()
-print topn_liq.tail()
-"""
+today_data_all = {}
+lkbk_days_data_all = {}
+multiplier = {}
 
-# kNN
-knn = select_model.KNN(topn_liq, qqq)
-st = time.time()
-print 'start error_score (lkbk = %d, ntop = %d, k = %d)' % (lkbk, ntop, k)
-error = knn.error_score(topn_liq, k)
-print '(lkbk = %d, ntop = %d, k = %d) error rate = %f%% [time = %fs]' % (lkbk, ntop, k, error, time.time()-st)
+for i in range(lkbk,60,lkbk+1):
+    today = training_set[i]
+    lkbk_days = training_set[i-lkbk-1:i]
+    print 'processing %s' % today
+    try:
+        today_time_range = day_time_range(today)
+            
+        vols = nasdaq_comp.ix[:, today, 'Volume']
+        closes = nasdaq_comp.ix[:, today, 'Close']
+        liqs = vols * closes
+        liqs = liqs / liqs.sum()
+        liqs = liqs.fillna(0)
         
-utils.save_object(store, topn_liq, 'top%d_liq' % ntop)
+        liqs = liqs[np.argsort(liqs)[::-1]]
+        topn_nasdaq = liqs.index[:]
+        
+        multiplier[today] = liqs[topn_nasdaq]
+        
+        pct_close = nasdaq_comp.ix[topn_nasdaq, today_time_range, '% Change(close)']
+        pct_close = pct_close * multiplier[today]
+        pct_liq_min = liq_mat.ix[today_time_range, topn_nasdaq].pct_change().fillna(0)
+        pct_liq_min = pct_liq_min * multiplier[today]
+        today_data_all[today] = Panel({
+                                '% Change(close)':pct_close,
+                                '% Change(liquidity)':pct_liq_min,}).transpose(2,0,1)
+                                
+        lkbk_days_range = None
+        for lkbk_day in lkbk_days:
+            mins = day_time_range(lkbk_day)
+            if lkbk_days_range is None:
+                lkbk_days_range = mins
+            else:
+                lkbk_days_range = lkbk_days_range.append(mins)
+                
+        pct_close = nasdaq_comp.ix[topn_nasdaq, lkbk_days_range, '% Change(close)']
+        pct_close = pct_close * multiplier[today]
+        pct_liq_min = liq_mat.ix[lkbk_days_range, topn_nasdaq].pct_change().fillna(0)
+        pct_liq_min = pct_liq_min * multiplier[today]
+        lkbk_days_data_all[today] = Panel({
+                                    '% Change(close)':pct_close,
+                                    '% Change(liquidity)':pct_liq_min,}).transpose(2,0,1)
+                                    
+    except:
+        print sys.exc_info()
+        print "no record found, maybe a holiday"
 
+# backup file of console prints, just in case
+f = open(dir_name+'/oost_'+start_day_str+'.txt', 'w')
+
+sum_err = {}
+ct = 0
+for k in range(11): sum_err[k] = 0
+
+for i in range(lkbk, 60, lkbk+1):
+    try:
+        today = training_set[i]
+        today_data = today_data_all[today].ix[:10,:,:]
+        lkbk_days_data = lkbk_days_data_all[today].ix[today_data.items,:,:]
+        
+        utils._print(f, '\n\n>>>>>>>>>>> set %d' % (i+1) )
+        utils._print(f, 'today = %s' % today )
+        utils._print(f, 'top10 nasdaq components : %s' % today_data.items )
+
+        close_name = '% Change(close)'
+        liq_name = '% Change(liquidity)'
+
+        today_close = today_data.ix[:,close_name,:]
+        today_liq = today_data.ix[:,liq_name,:]
+        today_close.columns = [(x + '_close') for x in today_close.columns]
+        today_liq.columns = [(x + '_liq') for x in today_liq.columns]
+
+        lkbk_close = lkbk_days_data.ix[:,close_name,:]
+        lkbk_liq = lkbk_days_data.ix[:,liq_name,:]
+        lkbk_close.columns = [(x + '_close') for x in lkbk_close.columns]
+        lkbk_liq.columns = [(x + '_liq') for x in lkbk_liq.columns]
+        
+        day_err = {}
+        test_set = today_close #concat([today_close, today_liq], axis=1)
+        train_set = lkbk_close #concat([lkbk_close, lkbk_liq], axis=1)
+        # kNN
+        knn = select_model.KNN(train_set, qqq)
+        for k in range(5,10):
+            st = time.time()
+            error = knn.error_score(test_set, k)
+            day_err[k] = error
+            sum_err[k] += error
+            utils._print(f,'(ntop=%d, lkbk=%d, k=%d) error = %.2f%% (time=%.2fs) date = %s' % (ntop, lkbk, k, error, time.time()-st, today) )
+        utils._print(f,'%s' % day_err)
+        ct += 1
+    except:
+        print sys.exc_info()
+
+utils._print(f, str(sum_err))
+for k in range(5,10):
+    utils._print(f,'%f' % (sum_err[k] / ct))
+f.close() 
 
